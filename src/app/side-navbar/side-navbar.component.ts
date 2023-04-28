a8f3de53f48df24a29d554b7b4d3ede65220e299
Salooma <saloomaali2000@gmail.com>
2023-04-28 16:14:44 +0530

    toggling sidebar created

---
+++ src/app/side-navbar/side-navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+declare var $: any;
+
+@Component({
+  selector: 'app-side-navbar',
+  templateUrl: './side-navbar.component.html',
+  styleUrls: ['./side-navbar.component.css']
+})
+export class SideNavbarComponent {
+
+
+}
+
+
