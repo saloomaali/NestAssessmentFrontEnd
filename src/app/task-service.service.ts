aa3f703e914389d6e85001f2817e7ecb8849ee7e
Salooma <saloomaali2000@gmail.com>
2023-05-10 21:46:13 +0530

    environment folder created

--- src/app/task-service.service.ts
+++ src/app/task-service.service.ts
@@ -1,11 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environment/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class TaskServiceService {
 
   constructor(private http: HttpClient) { }
-  viewTasks = () => { return this.http.get("http://localhost:8081/mytask/view") }
-  updateComment = (id: any, data: any) => { return this.http.put("http://localhost:8081/mytask/update/" + id, data) }
+  viewTasks = () => { return this.http.get(environment.viewTasksUrl) }
+  updateComment = (id: any, data: any) => { return this.http.put(environment.updateComment + id, data) }
 }
---
+++ src/environment/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    viewTasksUrl: "http://localhost:8081/mytask/view",
+    updateComment: "http://localhost:8081/mytask/update/"
+};
